8df05afdb82d23776a18d935189853077a4544bc
timothycarambat <rambat1010@gmail.com>
2018-05-23 22:22:00 -0500

    upgrade to fontawesome5

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -1,5 +1,12 @@
 require('./bootstrap');
 
+import fontawesome from '@fortawesome/fontawesome'
+import regular from '@fortawesome/fontawesome-free-regular'
+import solid from '@fortawesome/fontawesome-free-solid'
+
+fontawesome.library.add(regular)
+fontawesome.library.add(solid)
+
 
 require('./includes/bootstrap-checkbox-radio');
 require('./includes/bootstrap-notify');
