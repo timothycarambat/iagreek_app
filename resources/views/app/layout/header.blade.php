8666d18afc79a8d2dec076f4b285373da5feafe5
timothycarambat <rambat1010@gmail.com>
2018-05-27 21:15:08 -0500

    remove templating stuff

--- resources/views/app/layout/header.blade.php
+++ resources/views/app/layout/header.blade.php
@@ -27,7 +27,6 @@
                         <b class="caret"></b>
                     </a>
                     <ul class="dropdown-menu">
-                      <li><a href="#">Notification 1</a></li>
                       <li><a href="/logout">Logout</a></li>
                     </ul>
                 </li>
