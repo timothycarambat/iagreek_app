f1ab8a11d1a4ed3b96e97808e48ce211080001f5
timothycarambat <rambat1010@gmail.com>
2018-06-04 01:29:32 -0500

    member control bar move views around;

--- resources/views/app/members.blade.php
+++ resources/views/app/members.blade.php
@@ -32,7 +32,11 @@
 						@if( count($members) > 0 )
 							@foreach($members as $member)
 								<tr>
-									<td></td>
+									<td style="width:6%">
+											<a data-toggle='modal' data-target='' data-member='{{$member->id}}' style='cursor:pointer' title="Add Tags to Member"><i style='color:#3c763d' class="fa-fw fas fa-tags"></i></a>
+											<a data-toggle='modal' data-target='' data-member='{{$member->id}}' style='cursor:pointer' title="Edit Member Info"><i style='color:#31708f' class="fa-fw fas fa-user-edit"></i></a>
+											<a data-toggle='modal' data-target='' data-member='{{$member->id}}' style='cursor:pointer' title="Delete Member"><i style='color:#a94442' class="fa-fw fas fa-trash-alt"></i></a>
+									</td>
 									<td class="text-center">{{$member->name}}</td>
 									<td class="text-center">{{$member->email}}</td>
 									<td class="text-center">{{$member->status}}</td>
@@ -57,46 +61,8 @@
 			</div>
 	</div>
 
+@include('app.components.members.newMemberModal')
+@include('app.components.members.addTagsModal')
 
-	<!-- New Member Modal -->
-<div class="modal fade" id="newMemberModal" tabindex="-1" role="dialog" aria-hidden="true">
-  <div class="modal-dialog" role="document">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h4 class="modal-title text-center">Add New Members</h4>
-      </div>
-      <div class="modal-body">
-				<p class="well"> <strong>Hey!</strong> We offer two easy ways to add members. You can either download our template and reupload it with all
-					your members and we will update members that are already here and add the ones that aren't! You can also
-					add single inputs of members!
-					<br><br>Keep in mind that only active members count against your subscription count!</p>
-
-
-				<ul class="nav nav-tabs">
-				  <li class="nav-item active">
-				    <a class="nav-link active" href="#addByFile" data-toggle="tab">Add by File</a>
-				  </li>
-				  <li class="nav-item">
-				    <a class="nav-link" href="#addByForm" data-toggle="tab">Add by Form</a>
-				  </li>
-				</ul>
-
-				<div class="tab-content">
-	        <div class="tab-pane active" id="addByFile">
-					  @include('app.components.addByFile')
-	        </div>
-
-	        <div class="tab-pane" id="addByForm">
-						@include('app.components.addByForm')
-	        </div>
-      	</div>
-
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-      </div>
-    </div>
-  </div>
-</div>
 
 @endsection
