11d1ff0228839d11e5900314c5e8556449b4e85d
timothycarambat <rambat1010@gmail.com>
2018-06-20 23:37:59 -0500

    add remeber tokens to memebers

---
+++ database/migrations/2018_06_21_043631_add_remember_token_to_members.php
@@ -0,0 +1,21 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddRememberTokenToMembers extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+      Schema::table('members', function (Blueprint $table) {
+        $table->rememberToken();
+      });
+    }
+
+}
