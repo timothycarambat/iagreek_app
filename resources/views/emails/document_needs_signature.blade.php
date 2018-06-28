f261b7100de9a6e4e8300278758f135ae02a0169
uniti-tim <timothy.carambat@uniti.com>
2018-06-27 20:04:34 -0500

    update emails

--- resources/views/emails/document_needs_signature.blade.php
+++ resources/views/emails/document_needs_signature.blade.php
@@ -16,8 +16,11 @@
   			 <h3>Hey {{ucwords($model->name)}}!</h3>
 				 <p>We wanted to let you know that there a document that requires your signature for {{ucwords($org_name)}}
            <br>
-           You can do this from your phone or computer, whichever is easiet. Once click and done!
+           You can do this from your phone or computer, whichever is easiet. One click and done!
+					 <br>
+					 <a href="{{$_ENV['SIGN_URL']}}"> Click Here to Login </a>
          </p>
+				 	<a href="{{$_ENV['SIGN_URL']}}"> {{$_ENV['SIGN_URL']}} </a>
 				 <br>
 				 <p>Best, <br> IAGREEK Support Team </p>
 
