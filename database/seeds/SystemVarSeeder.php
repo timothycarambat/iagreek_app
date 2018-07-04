e263e8657d993a594b8c82c8bc3adc7d07047c75
timothycarambat <rambat1010@gmail.com>
2018-07-04 00:51:52 -0500

    revise seeder

--- database/seeds/SystemVarSeeder.php
+++ database/seeds/SystemVarSeeder.php
@@ -15,10 +15,10 @@ class SystemVarSeeder extends Seeder
         DB::table('system')->truncate();
         SystemVar::create([ 'name' => "trial_days", 'value' => "15" ]);
         SystemVar::create([ 'name' => "org_small", 'value' => "50"]);
-        SystemVar::create([ 'name' => "org_med", 'value' => "100"]);
+        SystemVar::create([ 'name' => "org_med", 'value' => "199"]);
         SystemVar::create([ 'name' => "org_large", 'value' => "Unlimited"]);
         SystemVar::create([ 'name' => "iag_small", 'value' => "50"]);
-        SystemVar::create([ 'name' => "iag_med", 'value' => "100"]);
+        SystemVar::create([ 'name' => "iag_med", 'value' => "199"]);
         SystemVar::create([ 'name' => "iag_large", 'value' => "Unlimited"]);
     }
 }
